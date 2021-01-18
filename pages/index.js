cd7f9b695ac69c139c238bc0c8ce8d9fa35655a1
Mário Dias <mario.sergiodias@icloud.com>
2021-01-18 11:27:14 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,6 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Mulher Eleita Já!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-     
-        <p className={styles.title}>
-          Welcome to Next.js!
-        </p>
-
- 
-      
-
-      <footer className={styles.footer}>
-      
-      </footer>
-    </div>
+    <div className={styles.container}></div>
   )
 }
