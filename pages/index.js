43b1394d45ecd21a81a300a9a74ae865ed9d9db7
Mário Dias <mario.sergiodias@icloud.com>
2021-01-18 12:11:08 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -7,18 +6,11 @@ export default function Home() {
     <div className={styles.container}>
     
     <p className={styles.title}>Mulher Eleita Já!</p>
-    <img src="/1.png" className={styles.imagens1}/>
-    <img src="/2.png" className={styles.imagens2}/>
-    <img src="/3.png" className={styles.imagens3}/>
-    <img src="/4.png" className={styles.imagens4}/>
-    <img src="/5.png" className={styles.imagens5}/>
-    
-    <Image
-  src="/1.png"
-  alt="Picture of a triangle"
-  width={400}
-  height={815.93}
-/>
+    <img src="/1.svg" className={styles.imagens1}/>
+    <img src="/2.svg" className={styles.imagens2}/>
+    <img src="/3.svg" className={styles.imagens3}/>
+    <img src="/4.svg" className={styles.imagens4}/>
+    <img src="/5.svg" className={styles.imagens5}/>
     
     </div>
   )
