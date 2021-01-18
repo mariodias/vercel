05a8e6e2ee3dcc670bbaeb11e861f90461281867
Mário Dias <mario.sergiodias@icloud.com>
2021-01-18 11:19:16 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Testing</title>
+        <title>Mulher Eleita Já!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
