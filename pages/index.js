bbfa988dce08b825548f734e8f9a2533509d6759
Mário Dias <mario.sergiodias@icloud.com>
2021-01-18 12:02:12 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -6,7 +7,18 @@ export default function Home() {
     <div className={styles.container}>
     
     <p className={styles.title}>Mulher Eleita Já!</p>
-    <img src="/1.png" className={styles.imagens}/>
+    <img src="/1.png" className={styles.imagens1}/>
+    <img src="/2.png" className={styles.imagens2}/>
+    <img src="/3.png" className={styles.imagens3}/>
+    <img src="/4.png" className={styles.imagens4}/>
+    <img src="/5.png" className={styles.imagens5}/>
+    
+    <Image
+  src="/public/1.png"
+  alt="Picture of a triangle"
+  width={400}
+  height={815.93}
+/>
     
     </div>
   )
